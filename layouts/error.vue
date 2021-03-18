f6c1a239b31a5ee11316d8b3ffeb74db85ac2db3
andri ferry <ferryandri35@gmail.com>
2021-03-18 16:06:39 +0700

    add error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,44 +1,58 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <div
+    class="w-full sm:h-smVh md:h-bigVh xl:h-bigVh lg:h-bigVh flex justify-center"
+  >
+    <div
+      class="container p-5 h-mobileSize sm:h-smVh md:h-bigVh xl:h-bigVh lg:h-bigVh w-full flex justify-center"
+    >
+      <div class="container my-auto items-center">
+        <div class="flex flex-col p-4 items-center">
+          <h1 class="text-9xl font-black capitalize p-4 text-primaryColor">
+            404
+          </h1>
+          <p
+            class="p-0 m-0 font-semibold text-primaryColor text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl text-center"
+          >
+            Sorry, the page you are looking for could not be found.
+          </p>
+
+          <div class="my-5">
+            <NuxtLink
+              to="/"
+              class="uppercase white--text font-bold p-4 rounded-full bg-green-400 flex items-center"
+            >
+              back to home
+            </NuxtLink>
+          </div>
+        </div>
+
+        <!-- <div class="flex justify-center p-4"></div> -->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  layout: 'empty',
+  layout: "empty",
   props: {
     error: {
       type: Object,
       default: null
     }
   },
-  data () {
+  data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
+      pageNotFound: "404 Not Found",
+      otherError: "An error occurred"
+    };
   },
-  head () {
+  head() {
     const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
       title
-    }
+    };
   }
-}
+};
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
