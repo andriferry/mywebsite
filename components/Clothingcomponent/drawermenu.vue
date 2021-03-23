e1193d7e633a5e3ad1e2297f714be8733a4fe6fd
andri ferry <ferryandri35@gmail.com>
2021-03-23 09:25:00 +0700

    success split drawer

---
+++ components/Clothingcomponent/drawermenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-navigation-drawer v-model="openMenu" right temporary fixed>
+    <v-list dense>
+      <v-list-item-group color="primary" v-model="group">
+        <v-list-item v-for="(menu, i) in menuList" :key="i">
+          <v-list-item-content>
+            <v-list-item-title
+              class="capitalize"
+              v-text="menu.text"
+            ></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>   
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: ["openMenu"],
+  data() {
+    return {
+      group: null,
+      menuList: [
+        { text: "about", link: "aboutme", target: "#aboutme" },
+        { text: "contact", link: "portfolio", target: "#portfolio" },
+        { text: "blog" },
+        { text: "shop" }
+      ]
+    };
+  },
+  watch: {
+    group() {
+      this.openMenu = false;
+    }
+  }
+};
+</script>
+
+<style></style>
