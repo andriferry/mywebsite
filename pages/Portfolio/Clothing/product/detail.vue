e7b3d5dc17944868994f296d751118f7e436bf34
andri ferry <ferryandri35@gmail.com>
2021-04-13 14:25:09 +0700

    add detail component

---
+++ pages/Portfolio/Clothing/product/detail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>This is detail component</div>
+</template>
+
+<script>
+export default {
+  layout(context) {
+    return "clothinglayout";
+  }
+};
+</script>
+
+<style></style>
