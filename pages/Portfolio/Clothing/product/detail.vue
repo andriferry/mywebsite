509866dc268fd6fe686e38a7d05288e4015a52fc
andri ferry <ferryandri35@gmail.com>
2021-04-13 16:27:22 +0700

    add title product

--- pages/Portfolio/Clothing/product/detail.vue
+++ pages/Portfolio/Clothing/product/detail.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>This is detail component</div>
+  <div class="w-full h-screen">
+    <v-img
+      eager
+      height="150"
+      src="https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+      lazy-src="https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+      aspect-ratio="2"
+    >
+      <div class="w-full h-full flex items-center">
+        <h1
+          class="flex font-bold uppercase text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center justify-center tracking-widest"
+        >
+          vinta travel bags
+        </h1>
+      </div>
+    </v-img>
+  </div>
 </template>
 
 <script>
