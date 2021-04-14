a3d5053512c7243f3aec8b6c6c4cee19ace81913
andri ferry <ferryandri35@gmail.com>
2021-04-14 09:03:44 +0700

    Add detail design product

--- pages/Portfolio/Clothing/product/detail.vue
+++ pages/Portfolio/Clothing/product/detail.vue
@@ -15,16 +15,49 @@
             vinta travel bags
           </h1>
 
-          <span
+          <spand
             class="flex justify-center font-bold capitalize text-white text-opacity-75 text-sm sm:text-lg lg:text-xl tracking-widest"
-            >t-shirt</span
+            >t-shirt</spand
           >
           <div class="w-full flex justify-center">
-            <v-breadcrumbs :items="items" divider="/"></v-breadcrumbs>
+            <v-breadcrumbs :items="items">
+              <template v-slot:item="{ item }">
+                <v-breadcrumbs-item nuxt to="/" :disabled="item.disabled">
+                  {{ item.text.toUpperCase() }}
+                </v-breadcrumbs-item>
+              </template>
+            </v-breadcrumbs>
           </div>
         </div>
       </div>
     </v-img>
+
+    <div class="p-1">
+      <div class="grid grid-rows-1 lg:grid-cols-2 gap-2">
+        <v-carousel height="350" hide-delimiters>
+          <v-carousel-item
+            eager
+            v-for="(item, i) in product"
+            :key="i"
+            :src="item.src"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          ></v-carousel-item>
+        </v-carousel>
+
+        <div class="text-white lg:col-span-1 p-4">
+          <div class="price flex justify-between items-center">
+            <span class="text-primaryColor text-3xl font-bold">$ 20.00</span>
+            <div class="">
+              <span class="text-secondaryColor text-lg font-bold"
+                >Free delivery</span
+              >
+              <v-icon class="" color="secondaryColor">mdi-truck-fast</v-icon>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -50,6 +83,20 @@ export default {
           text: "Link 2",
           disabled: true,
           href: "breadcrumbs_link_2"
+        }
+      ],
+      product: [
+        {
+          src:
+            "https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
+        },
+        {
+          src:
+            "https://images.unsplash.com/photo-1523380677598-64d85d015339?ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
+        },
+        {
+          src:
+            "https://images.unsplash.com/photo-1523380744952-b7e00e6e2ffa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
         }
       ]
     };
