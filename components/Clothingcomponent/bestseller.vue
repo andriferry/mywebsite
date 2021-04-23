52f52cd95dae1fa8404be6d938aec4c3ca0793a5
andri ferry <ferryandri35@gmail.com>
2021-04-23 09:15:22 +0700

    Add success looping on component bestseller

--- components/Clothingcomponent/bestseller.vue
+++ components/Clothingcomponent/bestseller.vue
@@ -17,7 +17,7 @@
           <NuxtLink
             to="/"
             class="h-auto hover:shadow-2xl transition duration-200 ease-out col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 justify-start flex flex-col"
-            v-for="(data, index) in dataImage"
+            v-for="(data, index) in dataProducts"
             :key="index"
           >
             <v-img :lazy-src="data.img" eager :src="data.img" height="300">
@@ -76,64 +76,7 @@
 <script>
 export default {
   data() {
-    return {
-      dataImage: [
-        {
-          img:
-            "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-          text: "nike red edition",
-          price: "50.00",
-          discount: 10
-        },
-        {
-          img:
-            "https://images.unsplash.com/photo-1547949003-9792a18a2601?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-          text: "vinta travel bags",
-          price: "30.00",
-          discount: 50
-        },
-        {
-          img:
-            "https://images.unsplash.com/photo-1539963011628-c076c7870cad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8&auto=format&fit=crop&w=900&q=60",
-          text: "saturdays cap",
-          price: "20.00",
-          discount: "out of stock"
-        },
-        {
-          img:
-            "https://images.unsplash.com/photo-1564859228273-274232fdb516?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80",
-          text: "classic white",
-          price: "10.00",
-          discount: 20
-        },
-        {
-          img:
-            "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
-          text: "UV protector sunglass",
-          price: "25.00"
-        },
-        {
-          img:
-            "https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80",
-          text: "minimalist watch",
-          price: "50.00",
-          discount: "out of stock"
-        },
-        {
-          img:
-            "https://images.unsplash.com/photo-1578237493287-8d4d2b03591a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8&auto=format&fit=crop&w=900&q=60",
-          text: "black hoope totebag",
-          price: "20.00"
-        },
-        {
-          img:
-            "https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-          text: "classic green",
-          price: "20.00",
-          discount: 15
-        }
-      ]
-    };
+    return {};
   },
   computed: {
     month() {
@@ -156,7 +99,9 @@ export default {
 
       return monthNames[month.getMonth()];
     },
-    dataProducts() {}
+    dataProducts() {
+      return this.$store.getters["dataProduct/product"];
+    }
   },
   mounted() {
     console.log(this.$store.getters["dataProduct/product"]);
