3526e78a5d81dca8ada484929535d4fcfe60db7c
andri ferry <ferryandri35@gmail.com>
2021-06-15 14:45:04 +0700

    Remove flex on action component

--- components/Action.vue
+++ components/Action.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="w-full h-auto p-4 mb-10" id="action">
     <div class="cardAction w-full sm:p-16">
-      <div class="container p-4 flex flex-col sm:flex-row">
-        <div class="action flex flex-col font-bold mb-2 mb-sm-0 text-2xl sm:text-3xl">
-          <span class="text-primaryColor">Got a project ?</span>
+      <div class="container p-4">
+        <div class="flex flex-col sm:flex-row">
+          <div
+            class="action flex flex-col font-bold mb-2 mb-sm-0 text-2xl sm:text-3xl"
+          >
+            <span class="text-primaryColor">Got a project ?</span>
 
-          <div class="w-auto">
-            <v-hover v-slot="{ hover }">
-              <NuxtLink
-                to="/"
-                class="secondaryColor--text hover:bg-primaryColor underline"
-                :class="{ 'white--text': hover }"
-                >Let's Talk !</NuxtLink
-              >
-            </v-hover>
+            <div class="w-auto">
+              <v-hover v-slot="{ hover }">
+                <NuxtLink
+                  to="/"
+                  class="secondaryColor--text hover:bg-primaryColor underline"
+                  :class="{ 'white--text': hover }"
+                  >Let's Talk !</NuxtLink
+                >
+              </v-hover>
+            </div>
           </div>
-        </div>
 
-        <div
-          class="email text-primaryColor text-xl sm:text-2xl font-medium flex items-center sm:justify-end"
-        >
-          <h1>contact@andriferry.com</h1>
+          <div
+            class="email text-primaryColor text-xl sm:text-2xl font-medium flex items-center sm:justify-end"
+          >
+            <h1>contact@andriferry.com</h1>
+          </div>
         </div>
       </div>
     </div>
