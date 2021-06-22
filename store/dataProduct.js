a974b6d211a286e874a41bc7b20a8fd8806e427f
andri ferry <ferryandri35@gmail.com>
2021-06-22 16:26:23 +0700

    download image and change dataProduct.js

--- store/dataProduct.js
+++ store/dataProduct.js
@@ -3,9 +3,7 @@ export default {
   state: () => ({
     products: [
       {
-        img: [
-          "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-        ],
+        img: ["nikere.webp"],
         id: 28,
         text: "nike red edition",
         price: "50.00",
@@ -24,9 +22,7 @@ export default {
         ]
       },
       {
-        img: [
-          "https://images.unsplash.com/photo-1547949003-9792a18a2601?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-        ],
+        img: ["vintatravelbag.webp"],
         id: 45,
         text: "vinta travel bags",
         price: "30.00",
@@ -45,9 +41,7 @@ export default {
         ]
       },
       {
-        img: [
-          "https://images.unsplash.com/photo-1539963011628-c076c7870cad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8&auto=format&fit=crop&w=900&q=60"
-        ],
+        img: ["saturdaycap.webp"],
         id: 56,
         text: "saturdays cap",
         price: "25.00",
@@ -66,9 +60,7 @@ export default {
         ]
       },
       {
-        img: [
-          "https://images.unsplash.com/photo-1564859228273-274232fdb516?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80"
-        ],
+        img: ["classicwhite.webp"],
         id: 70,
         text: "classic white",
         price: "10.00",
@@ -87,9 +79,7 @@ export default {
         ]
       },
       {
-        img: [
-          "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
-        ],
+        img: ["huvsunglass.webp"],
         id: 10,
         text: "UV protector sunglass",
         price: "25.00",
@@ -107,9 +97,7 @@ export default {
         ]
       },
       {
-        img: [
-          "https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80"
-        ],
+        img: ["minimalistwatch.webp"],
         id: 8,
         text: "minimalist watch",
         price: "50.00",
@@ -128,9 +116,7 @@ export default {
         ]
       },
       {
-        img: [
-          "https://images.unsplash.com/photo-1578237493287-8d4d2b03591a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8&auto=format&fit=crop&w=900&q=60"
-        ],
+        img: ["blacktotebag.webp"],
         id: 2,
         text: "black hoope totebag",
         price: "20.00",
@@ -149,11 +135,7 @@ export default {
       },
       {
         id: 7,
-        img: [
-          "https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-          "https://images.unsplash.com/photo-1523380677598-64d85d015339?ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
-          "https://images.unsplash.com/photo-1523380744952-b7e00e6e2ffa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-        ],
+        img: ["classicgreen1.webp", "classicgreen2.webp", "classicgreen3.webp"],
         text: "classic green",
         price: "20.00",
         slug: "classic-green",
