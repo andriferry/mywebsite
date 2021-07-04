34602ceff5dc92a9c770d9ca465376f631cf7e68
andri ferry <ferryandri35@gmail.com>
2021-07-04 16:27:49 +0700

    Add Composition API (Vuejs 3) Version

---
+++ components/TrackerCoronaComponent/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>this is navbar</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ layouts/coronalayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <!-- navbar -->
+    <TrackerCoronaComponentNavbar />
+    <!-- navbar -->
+    <v-main>
+      <!-- Main area -->
+      <nuxt />
+      <!-- Main area -->
+    </v-main>
+
+    <TrackerCoronaComponentFooter />
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
